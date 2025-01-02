da78575881e8ee6474397aa9f9bcecf541a901ec
Arif Hardian Pohan <ahpproject9@gmail.com>
2025-01-02 17:48:41 +0700

    config prettier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,9 +21,9 @@ import { ToggleMode } from "./ui/toogle-mode";
 
 export const NavBar = () => {
   return (
-    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+    <div className="supports-backdrop-blur:bg-white/60 sticky top-0 z-40 w-full flex-none bg-white/95 backdrop-blur transition-colors duration-500 dark:border-slate-50/[0.06] dark:bg-transparent lg:z-50 lg:border-b lg:border-slate-900/10">
       <div className="max-w-8xl mx-auto">
-        <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+        <div className="mx-4 border-b border-slate-900/10 py-4 dark:border-slate-300/10 lg:mx-0 lg:border-0 lg:px-8">
           <div className="relative flex items-center justify-between">
             {/* <TailwindSvg /> */}
             <Link className="mr-3 flex w-32 gap-2 overflow-hidden" href="/">
@@ -31,8 +31,8 @@ export const NavBar = () => {
               <ClockArrowUp /> Nextjs+Shadcn
             </Link>
 
-            <div className="relative hidden lg:flex items-center ml-auto">
-              <NavigationMenu className="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
+            <div className="relative ml-auto hidden items-center lg:flex">
+              <NavigationMenu className="text-sm font-semibold leading-6 text-slate-700 dark:text-slate-200">
                 <NavigationMenuList className="flex space-x-8">
                   <NavigationMenuItem>
                     <Link href="/docs" legacyBehavior passHref>
@@ -64,7 +64,7 @@ export const NavBar = () => {
                   </NavigationMenuItem>
                 </NavigationMenuList>
               </NavigationMenu>
-              <div className="flex items-center border-l border-slate-200 ml-6 pl-6 dark:border-slate-800">
+              <div className="ml-6 flex items-center border-l border-slate-200 pl-6 dark:border-slate-800">
                 <ToggleMode />
                 <Link
                   href="https://github.com/arifpohan12"
@@ -73,7 +73,7 @@ export const NavBar = () => {
                   <span className="sr-only">GitHub</span>
                   <svg
                     viewBox="0 0 16 16"
-                    className="w-5 h-5"
+                    className="h-5 w-5"
                     fill="currentColor"
                     aria-hidden="true"
                   >
@@ -83,18 +83,18 @@ export const NavBar = () => {
               </div>
             </div>
 
-            <div className="ml-2 -my-1 lg:hidden">
+            <div className="-my-1 ml-2 lg:hidden">
               <Dialog>
-                <DialogTrigger className="dialog__trigger text-slate-500 w-8 h-8 flex items-center justify-center hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300">
+                <DialogTrigger className="dialog__trigger flex h-8 w-8 items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300">
                   <span className="sr-only">Navigation</span>
                   <LucideMoreVertical />
                 </DialogTrigger>
                 <DialogPortal>
                   <DialogOverlay className="dialog__overlay inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
-                  <DialogContent className="dialog__content top-4 max-w-xs translate-x-[-46%] translate-y-0 bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900 dark:bg-slate-800 dark:text-slate-400 dark:highlight-white/5">
+                  <DialogContent className="dialog__content dark:highlight-white/5 top-4 max-w-xs translate-x-[-46%] translate-y-0 rounded-lg bg-white p-6 text-base font-semibold text-slate-900 shadow-lg dark:bg-slate-800 dark:text-slate-400">
                     <DialogTitle asChild />
 
-                    <DialogClose className="absolute top-5 right-5 w-8 h-8 flex items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"></DialogClose>
+                    <DialogClose className="absolute right-5 top-5 flex h-8 w-8 items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"></DialogClose>
 
                     <ul className="space-y-6">
                       <li>
@@ -138,9 +138,9 @@ export const NavBar = () => {
                         </a>
                       </li>
                     </ul>
-                    <DialogFooter className="pt-4 border-t border-slate-200 dark:border-slate-200/10">
+                    <DialogFooter className="border-t border-slate-200 pt-4 dark:border-slate-200/10">
                       <div className="flex items-center justify-between">
-                        <Label className="text-slate-700 font-normal dark:text-slate-400">
+                        <Label className="font-normal text-slate-700 dark:text-slate-400">
                           Switch theme
                         </Label>
                         <ToggleMode />
